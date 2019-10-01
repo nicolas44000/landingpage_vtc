59ee49110e5d7a530a63594a8f5b1b56dbc43c24
nicolas44000 <nicolaslucain@hotmail.fr>
2019-10-01 18:41:25 +0200

    bootstrap

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,29 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+import 'bootstrap'
+import 'packs/custom'
+import 'packs/plugins/easing/easing'
+import 'packs/plugins/lockfixed/lockfixed.min'
+import 'packs/plugins/counterup/counterup.min'
+import 'packs/plugins/font-awesome/css/font-awesome.min'
+import 'packs/plugins/parallax/parallax'
+import 'packs/plugins/stellar/stellar.min'
+import 'packs/plugins/stickyjs/sticky'
+import 'packs/plugins/superfish/superfish.min'
+import 'packs/plugins/tether/js/tether.js'
+import 'packs/plugins/waypoints/waypoints.min'
