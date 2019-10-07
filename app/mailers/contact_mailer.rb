650cdc619cb25ae0697767cac10af074f46d7e98
nicolas44000 <nicolaslucain@hotmail.fr>
2019-10-07 12:13:25 +0200

    tel

--- app/mailers/contact_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ContactsMailer < ApplicationMailer
-    default :from => 'contact@webconcepter.com'
-   
-    def contact_email(contact)
-      @contact = contact
-      mail( :to => 'contact@webconcepter.com',
-      :subject => 'Webconcepter | Agence Web | 39 Boulevard Albert Einstein, 44300 Nantes ' )
-    end
-  end
